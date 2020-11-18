1b1d0b7ee014f4050820fcd9c3e5989704e589a1
SzaboPeter20 <peterszabo121@gmail.com>
2020-11-18 23:54:30 +0100

    Pontozólap és egyéb finomítások

--- Script.js
+++ Script.js
@@ -6,3 +6,10 @@ function myFunction() {
       x.className = "topnav";
     }
 }
+
+function calcSum() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let num2 = document.getElementsByName("num2")[0].value;
+    let sum = Number(num1) + Number(num2);
+    document.getElementsByName("sum")[0].value = sum;
+}
